53db1dffe4af66b799949b56642482d279624641
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-08-09 07:52:50 -0300

    Kubernetes para Devs Backend - Parte #1

--- k8s/parte1/demo-quarkus/src/main/java/br/com/eduardomelle/GreetingResource.java
+++ k8s/parte1/demo-quarkus/src/main/java/br/com/eduardomelle/GreetingResource.java
@@ -4,13 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private static int i = 0;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String hello() throws UnknownHostException {
+        // return "Hello from RESTEasy Reactive";
+        return "Hello, Confraria Dev! V2: " + i++ + " - " + InetAddress.getLocalHost().getHostName() + "\n";
     }
+
 }
